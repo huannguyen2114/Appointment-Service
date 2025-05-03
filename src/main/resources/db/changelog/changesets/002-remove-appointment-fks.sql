43adf4698068a379029a3e97754b38d969c5bce0
huan.nguyen <huan.nguyen2114@gmail.com>
2025-05-03 08:08:59 +0700

    ok

--- src/main/resources/db/changelog/changesets/002-remove-appointment-fks.sql
+++ src/main/resources/db/changelog/changesets/002-remove-appointment-fks.sql
@@ -3,3 +3,7 @@ ALTER TABLE "appointment" DROP CONSTRAINT IF EXISTS fk_appointment_patient;
 ALTER TABLE "shift" DROP CONSTRAINT IF EXISTS shift_staff_id_fkey;
 ALTER TABLE "patient"
 ADD COLUMN "phone_number" VARCHAR(15);
+ALTER TABLE "patient"
+ADD COLUMN "created_at" timestamptz DEFAULT CURRENT_TIMESTAMP;
+ALTER TABLE "patient"
+ADD COLUMN "updated_at" timestamptz DEFAULT CURRENT_TIMESTAMP;
